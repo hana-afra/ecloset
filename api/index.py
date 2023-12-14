9e01918c28bdda088953165195140c8b2be02447
hana-afra <hana.afra.3003@gmail.com>
2023-12-14 15:11:07 +0100

    single endpoint

--- api/index.py
+++ api/index.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask, request
+import json
+from supabase import create_client, Client
 
 app = Flask(__name__)
+/
+#This is the very stupid way to store private/confidential data inside GIT
+#Store inside a file to be ignored
+url="https://jzredydxgjflzlgkamhn.supabase.co"
+key="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp6cmVkeWR4Z2pmbHpsZ2thbWhuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDIzMDQ1OTYsImV4cCI6MjAxNzg4MDU5Nn0.7jYAyMpWdzAnyioSoVkJiRFWzWyZvs0kdDxlli02er4"
+supabase: Client = create_client(url, key)
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
-
-@app.route('/about')
-def about():
-    return 'About'
+@app.route('/item.get')
+def api_item_get(): 
+   response = supabase.table('item').select("*").execute()
+   return json.dumps(response.data)
